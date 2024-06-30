abbaadc3b6aeddec6f73fdbaed30ed0c6a1adff8
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-07-01 05:29:00 +0600

    home page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-5xl">This is the home page</h1>
+      <h1 className="text-5xl">This is the home page updated</h1>
     </div>
   );
 };
